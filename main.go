9f8d41871a5f51587f845d8ae09b0fb418d0ae0e
iacguru <infraascodeguru@gmail.com>
2022-02-10 00:14:05 -0600

    initial commit

--- main.go
+++ main.go
@@ -1,16 +1,24 @@
 package main
 
 import (
-	"git/github"
-
-	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
-	"github.com/hashicorp/terraform-plugin-sdk/v2/plugin"
+	"github.com/summitdevops/custom_provider/client"
+	"github.com/summitdevops/custom_provider/github-client-go/github"
 )
 
+//"git/github"
+
+//"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+//"github.com/hashicorp/terraform-plugin-sdk/v2/plugin"
+
 func main() {
-	plugin.Serve(&plugin.ServeOpts{
-		ProviderFunc: func() *schema.Provider {
-			return github.Provider()
-		},
-	})
+	/*
+		plugin.Serve(&plugin.ServeOpts{
+			ProviderFunc: func() *schema.Provider {
+				return github.Provider()
+			},
+		})
+	*/
+	client.NewClient("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")
+	github.GetWorkflowByName("summitdevops", "github_workflow", "dispatch.yaml")
+
 }
